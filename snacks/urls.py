921a5de7c8970773bf36c49b76a1a535c621be59
emad-almajdalawi <majdalawi97@yahoo.com>
2022-05-16 20:48:42 +0300

    feat: delete

--- snacks/urls.py
+++ snacks/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import SnackListView, SnackDetailView, SnackCreateView, SnackUpdateView, SnackUpdateView
+from .views import SnackListView, SnackDetailView, SnackCreateView, SnackUpdateView, SnackDeleteView
 
 urlpatterns = [
     path('', SnackListView.as_view(), name='snack_list'),
     path('snacks-details/<int:pk>', SnackDetailView.as_view(), name='snack_detail'),
     path('create-snack/', SnackCreateView.as_view(), name='snack_new'),
     path('update-snack/<int:pk>', SnackUpdateView.as_view(), name='snack_edit'),
-    path('delete-snack/<int:pk>', SnackUpdateView.as_view(), name='snack_delete'),
+    path('delete-snack/<int:pk>', SnackDeleteView.as_view(), name='snack_delete'),
 ]
