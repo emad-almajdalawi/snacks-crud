c4b6c95bea420931d755a2f8b07ccd9fc2ae47f2
emad-almajdalawi <majdalawi97@yahoo.com>
2022-05-16 15:09:22 +0300

    feat! : Views, templates

--- snacks/urls.py
+++ snacks/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
+from .views import SnackListView, SnackDetailView, SnackCreateView, SnackUpdateView, SnackUpdateView
 
 urlpatterns = [
-    
+    path('', SnackListView.as_view(), name='snack_list'),
+    path('snacks-details/<int:pk>', SnackDetailView.as_view(), name='snack_detail'),
+    path('create-snack/', SnackCreateView.as_view(), name='snack_new'),
+    path('update-snack/<int:pk>', SnackUpdateView.as_view(), name='snack_edit'),
+    path('delete-snack/<int:pk>', SnackUpdateView.as_view(), name='snack_delete'),
 ]
--- snacks/views.py
+++ snacks/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import Snack
+# from django.db import models
 
-# Create your views here.
+
+class SnackListView(ListView):
+    model = Snack
+    template_name = 'snack_list.html'
+    context_object_name = 'order_object'
+
+    # def get_queryset(self):
+    #     return models.Snack.objects.all()
+
+class SnackDetailView(DetailView):
+    model = Snack
+    template_name = 'snack_detail.html'
+
+class SnackCreateView(CreateView):
+    model = Snack
+    template_name = 'snack_new.html'
+    fields = ['title', 'purchaser', 'description']
+
+
+class SnackUpdateView(UpdateView):
+    model = Snack
+    template_name = 'snack_edit.html'
+    fields = ['title', 'description']
+
+
+class SnackDeleteView(DeleteView):
+    model = Snack
+    template_name = 'snack_delete.html'
+    success_url = '/'
