e24a9d5a9c5009e3439984155e40a4cdd6d9c987
emad-almajdalawi <majdalawi97@yahoo.com>
2022-05-16 16:18:22 +0300

    fix: style, static collect

--- snacks_crud_project/settings.py
+++ snacks_crud_project/settings.py
@@ -118,10 +118,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
 STATICFILES_DIRS = [
     BASE_DIR / "static",
-    '/var/www/static/',
 ]
+#for deployment
+# STATIC_ROOT = BASE_DIR / "static"
+
 
 
 # Default primary key field type
